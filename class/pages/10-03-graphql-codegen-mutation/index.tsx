f2a986252a27139c7afff3164f360e2f6afc98d8
nwd0907 <nwd0907@newbizstart.com>
2022-11-11 15:45:48 +0900

    Update codegen

--- class/pages/10-03-graphql-codegen-mutation/index.tsx
+++ class/pages/10-03-graphql-codegen-mutation/index.tsx
@@ -14,7 +14,7 @@ const 나의그래프큐엘셋팅 = gql`
 `
 
 export default function GraphqlMutationPage(){
-    const [counter, setCounter] = useState<number>(0)
+    // const [counter, setCounter] = useState<number>(0)
 
     // const [나의함수] = useMutation<결과타입, 변수타입>(나의그래프큐엘셋팅)
     const [나의함수] = useMutation<Pick<IMutation, "createBoard">, IMutationCreateBoardArgs>(나의그래프큐엘셋팅)
