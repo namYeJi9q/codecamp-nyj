1c6befd51bcc88daf2ec0747840be72828d2554b
namYeJi9q <nyj5850@naver.com>
2022-12-21 12:54:24 +0900

    test

--- class_build/pages/index.js
+++ class_build/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            <code className={styles.code}>pages1/index.tsx</code>
           </p>
           <div>
             <a
